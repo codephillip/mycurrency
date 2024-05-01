9237c762116372c6be7796ebce0e3282234473c6
codephillip <codephillip@gmail.com>
2024-05-02 00:21:55 +0300

    refactored controller functionality into services

--- app/services/currency_service.py
+++ app/services/currency_service.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
+
 from app.models import CurrencyExchangeRate
 
-def currency_converter(source_currency: str, amount: float, exchanged_currency: str):
+
+def convert_currency(source_currency: str, amount: float, exchanged_currency: str):
     try:
         exchange_rate = CurrencyExchangeRate.objects.filter(
             source_currency__code=source_currency.upper(),
@@ -11,3 +14,32 @@ def currency_converter(source_currency: str, amount: float, exchanged_currency: 
 
     exchanged_amount = amount * exchange_rate.rate_value
     return exchanged_amount, exchange_rate.rate_value
+
+def currency_converter_response(source_currency, amount, exchanged_currency):
+    result = convert_currency(source_currency, amount, exchanged_currency)
+    if result:
+        exchanged_amount, exchange_rate = result
+        return {
+            'source_currency': source_currency,
+            'amount': amount,
+            'exchanged_currency': exchanged_currency,
+            'exchanged_amount': exchanged_amount,
+            'exchange_rate': exchange_rate
+        }
+    return None
+
+
+def get_currency_exchanges(date_from, date_to, source_currency):
+    currency_exchanges = CurrencyExchangeRate.objects.filter(
+        valuation_date__range=(date_from, date_to),
+        source_currency__code=source_currency
+    ).select_related('exchanged_currency')
+
+    response_data = defaultdict(list)
+    for currency_exchange in currency_exchanges:
+        currency_code = currency_exchange.exchanged_currency.code
+        response_data[currency_code].append({
+            'date': str(currency_exchange.valuation_date),
+            'value': float(currency_exchange.rate_value)
+        })
+    return response_data
--- app/views.py
+++ app/views.py
@@ -1,10 +1,7 @@
-from collections import defaultdict
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import CurrencyExchangeRate
-
 from .serializers import CurrencyRateQueryParamsSerializer, CurrencyConverterSerializer
-from .services.currency_service import currency_converter
+from .services.currency_service import get_currency_exchanges, currency_converter_response
 
 
 class CurrencyRatePerCurrencyView(APIView):
@@ -15,20 +12,7 @@ class CurrencyRatePerCurrencyView(APIView):
             date_from = validated_data['date_from']
             date_to = validated_data['date_to']
             source_currency = validated_data['source_currency'].upper()
-
-            currency_exchanges = CurrencyExchangeRate.objects.filter(
-                valuation_date__range=(date_from, date_to),
-                source_currency__code=source_currency
-            ).select_related('exchanged_currency')
-
-            response_data = defaultdict(list)
-            for currency_exchange in currency_exchanges:
-                currency_code = currency_exchange.exchanged_currency.code
-                response_data[currency_code].append({
-                    'date': str(currency_exchange.valuation_date),
-                    'value': float(currency_exchange.rate_value)
-                })
-
+            response_data = get_currency_exchanges(date_from, date_to, source_currency)
             return Response({'data': {'source': source_currency, 'exchanges': response_data}})
         return Response(status=400, data=serializer.errors)
 
@@ -41,17 +25,8 @@ class CurrencyConverterView(APIView):
             source_currency = data['source_currency'].upper()
             amount = data['amount']
             exchanged_currency = data['exchanged_currency'].upper()
-
-            result = currency_converter(source_currency, amount, exchanged_currency)
-            if result:
-                exchanged_amount, exchange_rate = result
-                response_data = {
-                    'source_currency': source_currency,
-                    'amount': amount,
-                    'exchanged_currency': exchanged_currency,
-                    'exchanged_amount': exchanged_amount,
-                    'exchange_rate': exchange_rate
-                }
+            response_data = currency_converter_response(source_currency, amount, exchanged_currency)
+            if response_data:
                 return Response({'data': response_data})
             return Response(status=400, data={'error': 'Failed to convert'})
         return Response(status=400, data=serializer.errors)
